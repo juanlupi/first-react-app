ebfca945b2f7f58b325147934935be03e91dcc7e
Juan Lupi <juan.lupi@correo.ucu.edu.uy>
2023-07-21 13:52:42 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Components/Create';
+import BlogDetails from './Components/BlogDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home /> 
+            </Route>
+            <Route exact path="/create">
+              <Create /> 
+            </Route>
+            <Route exact path="/blogs/:id">
+              <BlogDetails /> 
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
