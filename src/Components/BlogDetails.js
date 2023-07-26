45934f35584efd4ed9beed4b5f942be25ca2e343
Juan Lupi <juan.lupi@correo.ucu.edu.uy>
2023-07-26 15:57:42 -0300

    BlogDetails added

--- src/Components/BlogDetails.js
+++ src/Components/BlogDetails.js
@@ -1,11 +1,32 @@
 import { useParams } from 'react-router-dom';
+import useFecth from './useFetch';
+import { useHistory } from 'react-router-dom';
 
 const BlogDetails = () => {
-const { id } = useParams();
+    const { id } = useParams();
+    const {data: blog, error, isPending } = useFecth('http://localhost:8000/blogs/' + id);
+    const history = useHistory();
+    
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+    }
 
     return ( 
         <div className="blog-details">
-            <h2>Blog Details - { id }</h2>
+            { isPending && <div>Loading...</div> }
+            { error && <div> { error } </div> }
+            { blog && (
+                <article>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                    <div>{ blog.body }</div>
+                    <button onClick={handleClick}>Delete</button>
+                </article>
+            ) }
         </div>
      );
 }
